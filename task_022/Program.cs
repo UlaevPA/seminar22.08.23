5ea15ac7a38aeac81781b4c74dfc2a3591061aa6
ulaev <ulaev1990@mail.ru>
2023-08-22 21:43:39 +0300

    Add task 22

--- task_022/Program.cs
+++ task_022/Program.cs
@@ -2,12 +2,24 @@
 // принимает на вход число (N) и выдаёт таблицу
 // квадратов чисел от 1 до N.
 
+
+void WriteTableSquare(int number)
+{
+    int i =1;
+    while (i <= number)
+    {
+        Console.WriteLine($"{i,3}-> {i * i,5}");
+        i++;
+    }
+}
 Console.WriteLine("Введите целое число");
 int n = Convert.ToInt32(Console.ReadLine());
-int i = 0;
+// int i = 0;
 
-while (i <= n)
-{
-    Console.WriteLine($"{i,3}-> {i * i,5}");
-    i++;
-}
+// while (i <= n)
+// {
+//     Console.WriteLine($"{i,3}-> {i * i,5}");
+//     i++;
+// }
+WriteTableSquare(n);
+
